cb7568d8b642e4b6b9fe2ee4efac6543bb3da938
phiemancraft <phiemancraft@gmail.com>
2020-10-13 10:11:05 +0700

    - Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 print('Hello World!')
 
 try:
-    r = requests.get('https://google.com')
+    r = requests.get('https://goo gle.com')
     print(r.status_code)
     if r.status_code == 200:
         print(r.text)
